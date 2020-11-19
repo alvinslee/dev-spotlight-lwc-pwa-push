ba595b90f9e322d1e700ff4c49c8584a8b6a5090
Alvin Lee <alvin@ootbdev.com>
2020-11-18 17:23:38 -0700

    Adds pushSW.js service worker, ensuring it is built to dist and registered in index.html

--- client/lwc-services.config.js
+++ client/lwc-services.config.js
@@ -5,6 +5,7 @@ module.exports = {
         { from: 'src/resources/', to: 'dist/resources/' },
 
         { from: 'src/index.html', to: 'dist/' },
-        { from: 'src/manifest.json', to: 'dist/' }
+        { from: 'src/manifest.json', to: 'dist/' },
+        { from: 'src/pushSW.js', to: 'dist/pushSW.js' }
     ]
 };
